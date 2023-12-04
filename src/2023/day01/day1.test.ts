2237f65b36f8432a9adf22550143072aaca779e9
Jarrod Zywien <jarrod.zywien@moodys.com>
2023-12-04 16:18:19 -0500

    implements day 1

--- src/2023/day01/day1.test.ts
+++ src/2023/day01/day1.test.ts
@@ -2,31 +2,32 @@ import { getInput } from '../../util/file';
 import { step1, step2 } from './day1';
 
 describe('day 1', () => {
-   let [input, sample] = ['', ''];
+   let [input, sample, sample2] = ['', '', ''];
    beforeEach(async () => {
       sample = await getInput(__dirname, 'sample');
+      sample2 = await getInput(__dirname, 'sample2');
       input = await getInput(__dirname, 'input');
    });
 
    describe('step 1', () => {
       test('sample', () => {
          const result = step1(sample);
-         expect(result).toEqual(24000);
+         expect(result).toEqual(142);
       });
       test('input', () => {
          const result = step1(input);
-         expect(result).toEqual(67016);
+         expect(result).toEqual(53386);
       });
    });
 
    describe('step 2', () => {
       test('sample', () => {
-         const result = step2(sample);
-         expect(result).toEqual(45000);
+         const result = step2(sample2);
+         expect(result).toEqual(281);
       });
       test('input', () => {
          const result = step2(input);
-         expect(result).toEqual(200116);
+         expect(result).toEqual(53312);
       });
    });
 });
--- src/2023/day01/day1.ts
+++ src/2023/day01/day1.ts
@@ -1,15 +1,35 @@
 import '@util/polyfills';
 
+const isNumber = (c: string): boolean => /^\d$/.test(c);
+const firstNumber = (chars: string[]) => parseInt(chars.find((c) => isNumber(c)) ?? '0');
+const numberMap = new Map<string, string>([
+   ['one', 'o1e'],
+   ['two', 't2o'],
+   ['three', 't3e'],
+   ['four', 'f4r'],
+   ['five', 'f5e'],
+   ['six', 's6x'],
+   ['seven', 's7n'],
+   ['eight', 'e8t'],
+   ['nine', 'n9e'],
+]);
+
 export const step1 = (input: string): number =>
    input
-      .splitAndGroup((item) => parseInt(item, 10))
-      .map((group) => group.sum())
-      .max();
+      .lines()
+      .map((line) => line.split(''))
+      .map((chars) => [firstNumber(chars), firstNumber(chars.reverse())])
+      .reduce((sum, [first, last]) => sum + first * 10 + last, 0);
 
 export const step2 = (input: string): number =>
    input
-      .splitAndGroup((item) => parseInt(item, 10))
-      .map((group) => group.sum())
-      .desc()
-      .take(3)
-      .sum();
+      .lines()
+      .map((line) => {
+         [...numberMap.entries()].forEach(([key, val]) => {
+            line = line.replaceAll(key, val);
+         });
+         return line;
+      })
+      .map((line) => line.split(''))
+      .map((chars) => [firstNumber(chars), firstNumber(chars.reverse())])
+      .reduce((sum, [first, last]) => sum + first * 10 + last, 0);
