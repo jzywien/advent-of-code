15755c363447c66fa2ebd09b658318364067002c
Jarrod Zywien <jarrod.zywien@moodys.com>
2024-12-03 11:56:50 -0500

    day 3 implementation

--- src/2024/day03/day3.test.ts
+++ src/2024/day03/day3.test.ts
@@ -11,22 +11,26 @@ describe('day 3', () => {
    describe('step 1', () => {
       test('sample', () => {
          const result = step1(sample);
-         expect(result).toBe(-1);
+         expect(result).toBe(161);
       });
       test('input', () => {
          const result = step1(input);
-         expect(result).toBe(-1);
+         expect(result).toBe(173529487);
       });
    });
 
    describe('step 2', () => {
+      beforeEach(async () => {
+         sample = await getInput(__dirname, 'sample2');
+      });
+
       test('sample', () => {
          const result = step2(sample);
-         expect(result).toBe(-1);
+         expect(result).toBe(48);
       });
       test('input', () => {
          const result = step2(input);
-         expect(result).toBe(-1);
+         expect(result).toBe(99532691);
       });
    });
 });
--- src/2024/day03/day3.ts
+++ src/2024/day03/day3.ts
@@ -1,5 +1,19 @@
 import '../../util/polyfills';
 
-export const step1 = (input: string): number => 0;
+export const step1 = (input: string): number => [...input.matchAll(/mul\(\d+,\d+\)/g)]
+   .map(item => item[0])
+   .map(item => [...item.matchAll(/\d+/g)].map(Number))
+   .map(([lhs, rhs]) => lhs * rhs)
+   .sum();
 
-export const step2 = (input: string): number => 0;
+export const step2 = (input: string): number => [...input.matchAll(/(mul\(\d+,\d+\))|(don't\(\))|(do\(\))/g)]
+   .map(item => item[0])
+   .reduce((state, command) => {
+      if (command.startsWith('don')) state.dont = true;
+      else if (command.startsWith('do')) state.dont = false;
+      if (command.startsWith('mul') && !state.dont) state.list.push(command);
+      return { ...state };
+   }, {dont: false, list: [] as string[]})
+   .list.map(item => [...item.matchAll(/\d+/g)].map(Number))
+   .map(([lhs, rhs]) => lhs * rhs)
+   .sum();
