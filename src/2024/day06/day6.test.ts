f598f1f6b33616e94c7b3a4ce1522587dc951013
Jarrod Zywien <jarrod.zywien@moodys.com>
2024-12-06 16:05:47 -0500

    ew… day 6 implementation
    step 2 is 13 seconds :(

---
+++ src/2024/day06/day6.test.ts
@@ -0,0 +1,32 @@
+import { getInput } from '@util/file';
+import { step1, step2 } from '../day06/day6';
+
+describe('day 6', () => {
+   let [input, sample, sample2] = ['', '', ''];
+   beforeEach(async () => {
+      sample = await getInput(__dirname, 'sample');
+      input = await getInput(__dirname, 'input');
+   });
+
+   describe('step 1', () => {
+      test('sample', () => {
+         const result = step1(sample);
+         expect(result).toEqual(41);
+      });
+      test('input', () => {
+         const result = step1(input);
+         expect(result).toEqual(5131);
+      });
+   });
+
+   describe('step 2', () => {
+      test('sample', () => {
+         const result = step2(sample);
+         expect(result).toEqual(6);
+      });
+      test('input', () => {
+         const result = step2(input);
+         expect(result).toEqual(-1);
+      });
+   });
+});
---
+++ src/2024/day06/day6.ts
@@ -0,0 +1,89 @@
+import '@util/polyfills';
+
+type Position = [number, number];
+type Dimension = [number, number];
+
+const direction = new Map([['^', [-1, 0]], ['>', [0, 1]], ['v', [1, 0]], ['<', [0, -1]]]);
+const turn = new Map([['^', '>'], ['>', 'v'], ['v', '<'], ['<', '^']]);
+
+interface Guard {
+   position: Position,
+   direction: string;
+}
+
+const findGuard = (grid: string[][]): Guard => {
+   for(let r = 0; r < grid.length; r++) {
+      for(let c = 0; c < grid[r].length; c++) {
+         if (grid[r][c] === '^') {
+            return {
+               position: [r, c],
+               direction: '^'
+            }
+         }
+      }
+   }
+   throw new Error('guard not found');
+}
+
+const inBounds = ([m, n]: Dimension, [x, y]: Position): boolean => x >= 0 && x < m && y >= 0 && y < n;
+
+export const step1 = (input: string): number => {
+   const grid = input.toMatrix<string>();
+   const guard = findGuard(grid);
+   const dim = [grid.length, grid[0].length] as Position;
+   const visited = new Set<string>();
+
+   while(inBounds(dim, guard.position)) {
+      const [dr, dc] = direction.get(guard.direction)!;
+      const [r, c] = guard.position;
+      const [r2, c2] = [r + dr, c + dc];
+      if (!inBounds(dim, [r2, c2])) break;
+      if (grid[r2][c2] === '#') {
+         guard.direction = turn.get(guard.direction)!;
+      } else {
+         guard.position = [r2, c2];
+         visited.add(`${r2},${c2}`);
+      }
+   }
+
+   return visited.size;
+};
+
+export const step2 = (input: string): number => {
+   const grid = input.toMatrix<string>();
+   const dim = [grid.length, grid[0].length] as Position;
+   let loopsDetected = 0;
+
+   for(let r = 0; r < grid.length; r++) {
+      for(let c = 0; c < grid[r].length; c++) {
+         if (grid[r][c] === '^' || grid[r][c] === '#') continue;
+
+         const gridCopy = input.toMatrix<string>();
+         const guard = findGuard(gridCopy);
+         gridCopy[r][c] = '#';
+
+         const visited = new Set<string>([`${guard.position[0]},${guard.position[1]},${guard.direction}`]);
+         while(inBounds(dim, guard.position)) {
+            const [dr, dc] = direction.get(guard.direction)!;
+            const [r, c] = guard.position;
+            const [r2, c2] = [r + dr, c + dc];
+            if (!inBounds(dim, [r2, c2])) break;
+
+            const key = `${r2},${c2},${guard.direction}`;
+            if (visited.has(key)) {
+               loopsDetected++;
+               break;
+            }
+
+            if (gridCopy[r2][c2] === '#') {
+               guard.direction = turn.get(guard.direction)!;
+            } else {
+               guard.position = [r2, c2];
+               visited.add(key);
+            }
+         }
+      }
+   }
+
+   return loopsDetected;
+};
